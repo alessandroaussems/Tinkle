ba201727ac812666b270f5a35b1ea56a4ec6f2ed
Alessandro Aussems <alessandrossms@gmail.com>
2017-09-27 21:06:14 +0200

    BetterAfterLogin

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('welcome'); //defines pages to which user is redirected on login
+Route::get('/', 'HomeController@index')->name('home'); //defines pages to which user is redirected on login
 
 Route::get('/toilets', function () {
     // Only authenticated users may enter...

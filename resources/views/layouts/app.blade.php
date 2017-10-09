464522706f4dfb925031c465f00c1e9c7816cbd3
Alessandro Aussems <alessandrossms@gmail.com>
2017-10-09 18:33:45 +0200

    AddedFavico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
+    <link rel="icon" type="image/png" href="{{ asset('img/MapLogo.png')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>{{ config('app.name', 'Tinkle') }}</title>
